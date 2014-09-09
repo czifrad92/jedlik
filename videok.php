8e55e386a6a1e9d643f07b9508e56019f27946fa
czifrad92 <czifrad92@gmail.com>
2014-09-09 16:56:22 +0200

    Jedlik 1.05
    - Videók bedesignolva

--- videok.php
+++ videok.php
@@ -7,23 +7,28 @@ include 'core/connect.php';
 
 fejlec_letrehozas("");
 
-$sql = "SELECT * FROM videok";
+$sql = "SELECT * FROM videok order by id DESC";
 
 $res = mysqli_query($connect,$sql);
 			while ($a = mysqli_fetch_assoc($res)) {
 
 			$code = str_replace('watch?v=', 'embed/', $a['link']);
+			$keywords = str_replace(',', '&nbsp;&nbsp;&nbsp;', $a['tag']);
+			$kiserlet = str_replace(', ', '<br>', $a['kiserlet']);
 			
 ?>
-
+<h1>Videók</h1>
 
 <div class="videocontainer">
 	<div class="videoleft"><iframe width="520" height="315" src="<?php echo $code; ?>"></iframe></div>
 	<div class="videoright">
-		<?php echo $a['cim']; ?>
-		<?php echo $a['tag']; ?>
-		<?php echo $a['iskola']; ?>
-		<?php echo $a['kiserlet']; ?>
+		<div class="videotitle"><?php echo $a['cim']; ?></div>
+		<b><?php echo $a['iskola']; ?></b><br><br>
+		
+
+		<?php echo $kiserlet; ?>
+
+		<div class="videotags"><?php echo $keywords; ?></div>
 	</div>
 </div>
 
